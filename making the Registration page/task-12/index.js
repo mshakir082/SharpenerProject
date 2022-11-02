0a9c1dc5b42c6b2db0ef6a2f9a64115dacf45747
mshakir082 <mdshakirhussain298@gmail.com>
2022-11-02 14:46:05 +0530

    get Data (axios)

---
+++ making the Registration page/task-12/index.js
@@ -0,0 +1,95 @@
+window.addEventListener("load",()=>{
+  axios.get('https://crudcrud.com/api/fb2570580d0e4d048f9853a780a9a671/appoinmentData')
+  .then(res=>{
+      console.log(res.data)
+      displayData(res.data)
+  })
+  .catch(err=>console.log(err));
+})
+
+const displayData = (formData) => {
+
+  document.getElementById('items').innerHTML = null;
+  formData.map((el, index) => {
+    var divflex = document.createElement('div');
+    var div1 = document.createElement('div');
+    div1.setAttribute('style', 'display:flex;justify-content:left;');
+    divflex.setAttribute('style', 'display:flex;justify-content:space-between;');
+    var list = document.createElement('li');
+    list.setAttribute('class', 'list-group-item')
+    var p1 = document.createElement('h2');
+    var p2 = document.createElement('h2');
+    var p3 = document.createElement('h2');
+
+    p1.innerText = `${el.expenseamount} -`;
+    p2.innerText = `${el.description} -`;
+    p3.innerText = el.category;
+    div1.append(p1, p2, p3);
+    var div2 = document.createElement('div');
+    div2.setAttribute('style', 'display:flex;gap:15px;');
+    let btnDelete = document.createElement('button');
+    btnDelete.innerText = 'Expense delete';
+    btnDelete.addEventListener('click', () => {
+      
+     axios.delete(`https://crudcrud.com/api/466d15103dde4c099257557411469f57/appoinmentData/${el._id}`)
+      .then(res=>console.log(res))
+      
+    })
+    let btnEdit = document.createElement('button');
+
+    btnEdit.innerText = 'Expense Edit'
+    document.getElementById('udateBtn').style.display = "none";
+
+    btnEdit.addEventListener('click', function () {
+
+      document.getElementById('submitBtn').style.display = "none";
+      document.getElementById('udateBtn').setAttribute('style', 'width:15%;');
+      document.getElementById('udateBtn').style.display = "block";
+      document.getElementById('expenseamount').value = el.expenseamount;
+      document.getElementById('description').value = el.description;
+      document.getElementById('category').value = el.category;
+
+      document.getElementById('udateBtn').addEventListener('click', function () {
+
+        document.getElementById('submitBtn').style.display = "block";
+        document.getElementById('udateBtn').style.display = "none";
+        formData[index].expenseamount = document.getElementById('expenseamount').value;
+        formData[index].description = document.getElementById('description').value;
+        formData[index].category = document.getElementById('category').value;
+       // localStorage.setItem('ExpenseData', JSON.stringify(formData));
+       // displayData(formData);
+
+      })
+    })
+    div2.append(btnDelete, btnEdit);
+    divflex.append(div1, div2)
+    list.append(divflex);
+    document.getElementById('items').append(list);
+  })
+}
+
+
+// Add new Data list------------------------------
+
+var form = document.querySelector('.form-inline');
+form.addEventListener('submit', (e) => {
+
+  e.preventDefault();
+  let items = document.querySelectorAll('.list-group-item');
+  let expenseamount = form.expenseamount.value;
+  let description = form.description.value;
+  let category = form.category.value;
+
+  let obj = {
+    expenseamount,
+    description,
+    category
+  }
+  axios.post("https://crudcrud.com/api/fb2570580d0e4d048f9853a780a9a671/appoinmentData",obj)
+  .then(res=>{
+    displayData([res.data]);
+    console.log(res);
+  })
+  .catch(err=>console.log(err))
+ 
+});
